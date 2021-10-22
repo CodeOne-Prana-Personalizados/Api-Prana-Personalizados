7bcc1da43071824282855d22efc6655065ffbf56
Petitevale <vrestrepol@unal.edu.co>
2021-10-22 12:42:37 -0500

    Se elimina el backend Sprint4

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 import express from "express"
 import cors from "cors"
 import producto from "./api/producto.route.js"
-
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://prana-personalizados.us.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'api-autenticación-prana-personalizados',
+  issuer: 'https://prana-personalizados.us.auth0.com/',
+  algorithms: ['RS256']
+});
+
+app.use(jwtCheck)
 
 
 app.use("/api/v1/producto", producto)
